eeb65ba5e23b2b199ec529536cdb9f9552dd6d21
Nate Delage <nate.delage@reverb.com>
2016-03-15 11:21:04 -0500

    Remove alexa

--- analytics.js
+++ analytics.js
@@ -7215,7 +7215,6 @@ module.exports = {
 
 module.exports = {
   'adwords': require('analytics.js-integration-adwords'),
-  'alexa': require('analytics.js-integration-alexa'),
   'google-analytics': require('analytics.js-integration-google-analytics'),
   'mixpanel': require('analytics.js-integration-mixpanel'),
   'facebook-pixel': require('analytics.js-integration-facebook-pixel')
